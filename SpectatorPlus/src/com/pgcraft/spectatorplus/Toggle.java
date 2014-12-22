222034e714d5d6bf54f0f0f47cc7dc8c2275bfbd
starg09 <ultraguille2006@gmail.com>
2014-12-22 10:11:12 -0300

    Add a Basic "Skript Integration Fix" Toggle
    This is kind of poorly made (new to java), so it probably misses
    something and may cause some bugs. But the reason is the following: In
    the SpectateListener, the general cancel on click events impedes Skript
    to handle it, thus disallowing for custom menu items (like a leave
    "button"). Thus, I first tried removing that single cancelation, and it
    actually worked, but I wanted to make it an optional toggle, and commit
    it to the original plugin, so more people can enjoy this plugin in a
    more customized way :)
    
    As mentioned in the yml option, this option should only be set to true
    if you're going to use Skript, or another plugin that will cancel and
    handle the click event. Otherwise, you'd allow people to interact with
    the world...

--- SpectatorPlus/src/com/pgcraft/spectatorplus/Toggle.java
+++ SpectatorPlus/src/com/pgcraft/spectatorplus/Toggle.java
@@ -51,6 +51,10 @@ public enum Toggle {
 	
 	SPECTATORS_TABLIST_PREFIX("spectators.tabListPrefix", Boolean.class, true, "Prefix spectator names in the tab list? This will change the Scoreboard used, and restore the old one when spectator mode is disabled. If you see another plugin's sidebar/infos on players list disappearing when you enable the spectator mode, try to disable this."),
 	SPECTATORS_SEE_OTHERS("spectators.spectatorsSeeSpectators", Boolean.class, true, "See other spectators when you're spectating? (*requires spectators.tabListPrefix to be true*)"),
+
+	// Skript Integration, for allowing click events...
+	
+	SKRIPT_INTEGRATION("skriptIntegration", Boolean.class, false, "Enable Skript integration? Only set to true if needed."),
 	
 	// What to do when the spectator mode is changed (enabled or disabled)?
 	
